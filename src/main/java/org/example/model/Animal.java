bc6ba0a9f3a6e43fa644faa06b828070b497d55b
ViorelBaciu <baciu_viorel94@yahoo.com>
2023-04-23 10:29:42 +0300

    add Animal and Food Models

---
+++ src/main/java/org/example/model/Animal.java
@@ -0,0 +1,40 @@
+package org.example.model;
+// reprezentare  a unei instante din tabelul "animals"
+public class Animal {
+    private Integer id; // nu folositi int pentru a evita NullPointerException
+    private String name;
+    private String species;
+
+    public Animal() {
+    }
+
+    public Animal(Integer id, String name, String species) {
+        this.id = id;
+        this.name = name;
+        this.species = species;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
